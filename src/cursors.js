84dcb91cb242db5b114b50780771db7c8698eaae
Pedro Machado Santa <pedro.santa@gmail.com>
2017-04-12 02:15:49 +0100

    Finish adding module code, add range fix dependency

--- src/cursors.js
+++ src/cursors.js
@@ -1,3 +1,5 @@
+import RangeFix from 'rangefix/rangefix';
+
 var DEFAULT_OPTIONS = {
   template: [
     '<span class="cursor-selections"></span>',
@@ -190,5 +192,82 @@ CursorsModule.prototype._shiftAll = function(index, length) {
 };
 
 CursorsModule.prototype._update = function(cursor) {
-  // TODO
+  if (!cursor || !cursor.range) return;
+
+  var containerRect = this.quill.container.getBoundingClientRect();
+  var startLeaf = this.quill.getLeaf(cursor.range.index);
+  var endLeaf = this.quill.getLeaf(cursor.range.index + cursor.range.length);
+  var range = document.createRange();
+  var rects;
+
+  // Sanity check
+  if (!startLeaf || !endLeaf ||
+    !startLeaf[0] || !endLeaf[0] ||
+    startLeaf[1] < 0 || endLeaf[1] < 0 ||
+    !startLeaf[0].domNode || !endLeaf[0].domNode) {
+    return this.hide(cursor);
+  }
+
+  range.setStart(startLeaf[0].domNode, startLeaf[1]);
+  range.setEnd(endLeaf[0].domNode, endLeaf[1]);
+  rects = RangeFix.getClientRects(range);
+
+  this._updateCaret(cursor, endLeaf, containerRect);
+  this._updateSelection(cursor, rects, containerRect);
 };
+
+CursorsModule.prototype._updateCaret = function(cursor, leaf) {
+  var rect, index = cursor.range.index + cursor.range.length;
+
+  // The only time a valid offset of 0 can occur is when the cursor is positioned
+  // before the first character in a line, and it will be the case that the start
+  // and end points of the range will be exactly the same... if they are not then
+  // a block selection is taking place and we need to offset the character position
+  // by -1;
+  if (index > 0 && leaf[1] === 0 && cursor.range.index !== (cursor.range.index + cursor.range.length)) {
+    index--;
+  }
+
+  rect = this.quill.getBounds(index);
+
+  cursor.caretEl.style.top = (rect.top) + 'px';
+  cursor.caretEl.style.left = (rect.left) + 'px';
+  cursor.caretEl.style.height = rect.height + 'px';
+
+  cursor.flagEl.style.top = (rect.top) + 'px';
+  cursor.flagEl.style.left = (rect.left) + 'px';
+};
+
+CursorsModule.prototype._updateSelection = function(cursor, rects, containerRect) {
+  function createSelectionBlock(rect) {
+    var selectionBlockEl = document.createElement('span');
+
+    selectionBlockEl.classList.add('selection-block');
+    selectionBlockEl.style.top = (rect.top - containerRect.top) + 'px';
+    selectionBlockEl.style.left = (rect.left - containerRect.left) + 'px';
+    selectionBlockEl.style.width = rect.width + 'px';
+    selectionBlockEl.style.height = rect.height + 'px';
+    selectionBlockEl.style.backgroundColor = tinycolor(cursor.color).setAlpha(0.3).toString();
+
+    return selectionBlockEl;
+  }
+};
+
+// Wipe the slate clean
+cursor.selectionEl.innerHTML = null;
+
+var index = [];
+var rectIndex;
+
+[].forEach.call(rects, function(rect) {
+  rectIndex = ('' + rect.top + rect.left + rect.width + rect.height);
+
+  // Note: Safari throws a rect with length 1 when caret with no selection.
+  // A check was addedfor to avoid drawing those carets - they show up on blinking.
+  if (!~index.indexOf(rectIndex) && rect.width > 1) {
+    index.push(rectIndex);
+    cursor.selectionEl.appendChild(createSelectionBlock(rect));
+  }
+}, this);
+
+//Quill.register('modules/cursor-highlight', CursorHighlight);
