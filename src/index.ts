b9970caa67b3c291db394ac3a67b17151f265080
Vladimir <vzh.inbox@gmail.com>
2020-01-27 17:27:32 +0600

    Add export Cursor

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import QuillCursors from './quill-cursors/quill-cursors';
 import '../assets/quill-cursors.scss';
+import Cursor from './quill-cursors/cursor';
 
 export default QuillCursors;
+
+export { Cursor };
