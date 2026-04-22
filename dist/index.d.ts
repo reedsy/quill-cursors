e07e3f448dec0592458e4eaf789c9374e08e0f00
Alec Gibson <12036746+alecgibson@users.noreply.github.com>
2026-04-22 09:37:33 +0100

    Release version v4.3.0

---
+++ dist/index.d.ts
@@ -0,0 +1,4 @@
+import QuillCursors from './quill-cursors/quill-cursors';
+import Cursor from './quill-cursors/cursor';
+import '../assets/quill-cursors.scss';
+export { QuillCursors as default, Cursor };
